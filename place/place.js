f1bae0cc00705ad640effc6ff17024670a66587e
Jorge Alvarez Alvarez - EVERIS <S56768@credito.bcp.com.pe>
2018-09-30 20:16:00 -0500

    Add place

---
+++ place/place.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const apiKey = 'AIzaSyA-HXVa2jtkGfKtIJwisxgC46RaWqC1xuI';
+
+const getAddress = async(address) => {
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${apiKey}`;
+
+    let response = await axios.get(url)
+
+    if (response.data.results.length === 0) {
+        throw new Error('No found results');
+    }
+
+    let result = response.data.results[0];
+    let coordinates = result.geometry.location;
+
+    return {
+        address: result.formatted_address,
+        latitude: coordinates.lat,
+        longitude: coordinates.lng
+    }
+}
+
+module.exports = {
+    getAddress
+}
